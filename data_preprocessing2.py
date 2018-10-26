eec79166fcb3b63d5e6586d4035b83251d0ada4a
MED-SALAH <32021669+MED-SALAH@users.noreply.github.com>
2018-10-26 20:13:18 +0200

    Update data_preprocessing2.py
    dataset

--- data_preprocessing2.py
+++ data_preprocessing2.py
@@ -6,3 +6,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importer le dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values # varialble indépondante
+y = dataset.iloc[:, -1].values # variable déponddante
+
+# Gérer les données manquantes
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NAN', strategy = 'mean', axis = 0)
